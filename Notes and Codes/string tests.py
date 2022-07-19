dfe8004a16f7fbbc2adf69ac5cc540a1fe790593
jjjeorgee <jjjeorgetheminimalist@gmail.com>
2022-07-19 20:06:11 +0100

    updates

---
+++ Notes and Codes/string tests.py
@@ -0,0 +1,15 @@
+spam = 'Hello world!!'
+print(spam[0])
+print(spam[6:]) 
+print(spam[:5])
+fizz = spam[0:5]
+print(fizz)
+
+'Hello' in 'Hello World'
+
+print('How are you?') 
+feeling = input() 
+if feeling.lower() == 'great':    
+    print('I feel great too.') 
+else:    
+    print('I hope the rest of your day is good.')
